ef70b7111a8b25a39fabe1b2f292e921b60ca3e6
Shubham Raj <shubhamaryan805@gmail.com>
2022-12-01 23:57:28 +0530

    Added : Ensure @ And Validate The Mandatory Part i.e. yahoo

--- src/main/java/com/regexemail/RegexValidation.java
+++ src/main/java/com/regexemail/RegexValidation.java
@@ -3,16 +3,23 @@ package com.regexemail;
 import java.util.regex.Pattern;
 
 public class RegexValidation {
+    /*
+     * @purpose: Ability to check valid regex
+     *
+     * @function : compiles given regex using pattren.compile method matches input
+     * and regex gives a boolean output
+     *
+     * @return: Returns true or false
+     */
 
     static public boolean isValid(String regex, String input) {
-
         Pattern pattern = Pattern.compile(regex);
         return Pattern.matches(regex, input);
 
     }
 
     public static void main(String[] args) {
-        boolean regexCheck = isValid("^([a-z]{1,10})$", "shubham");
+        boolean regexCheck = isValid("^[a-z]{1,10}[@][a-z]{1,10}$", "abc@yahoo");
         System.out.println(regexCheck);
     }
 }
